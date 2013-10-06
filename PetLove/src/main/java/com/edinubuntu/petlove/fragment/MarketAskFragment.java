a9e3e64a90a56058feb6424619cba4aee2c4d975
Edward Chiang <edward@thepolydice.com>
2013-10-06 14:49:52 +0800

    Design pet home fragment

--- PetLove/src/main/java/com/edinubuntu/petlove/fragment/MarketAskFragment.java
+++ PetLove/src/main/java/com/edinubuntu/petlove/fragment/MarketAskFragment.java
@@ -23,11 +23,12 @@ public class MarketAskFragment extends SherlockFragment {
     private Spinner choiceTypeSpinner;
     private Spinner choiceSexSpinner;
     private Spinner choiceAgeSpinner;
+    private Spinner choiceBuildSpinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_market_choice, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_market_ask, container, false);
 
         choiceTypeSpinner = (Spinner)rootView.findViewById(R.id.question_type_spinner);
         ArrayAdapter<String> choiceTypeAdapter = new ArrayAdapter<String>(getSherlockActivity(),
@@ -39,6 +40,7 @@ public class MarketAskFragment extends SherlockFragment {
         choiceSexSpinner = (Spinner)rootView.findViewById(R.id.question_sex_spinner);
         ArrayAdapter<String> choiceSexAdapter = new ArrayAdapter<String>(getSherlockActivity(),
                 R.layout.spinner_item_market_choice, new String[] {
+                getString(R.string.choice_ask_any),
                 getString(R.string.choice_ask_sex_female),
                 getString(R.string.choice_ask_sex_male)});
         choiceSexSpinner.setAdapter(choiceSexAdapter);
@@ -46,11 +48,24 @@ public class MarketAskFragment extends SherlockFragment {
         choiceAgeSpinner = (Spinner)rootView.findViewById(R.id.question_age_spinner);
         ArrayAdapter<String> choiceAgeAdapter = new ArrayAdapter<String>(getSherlockActivity(),
                 R.layout.spinner_item_market_choice, new String[] {
+                getString(R.string.choice_ask_any),
                 getString(R.string.choice_ask_age_young),
                 getString(R.string.choice_ask_age_adult),
                 getString(R.string.choice_ask_age_old) }
                 );
         choiceAgeSpinner.setAdapter(choiceAgeAdapter);
+
+        choiceBuildSpinner = (Spinner)rootView.findViewById(R.id.question_build_spinner);
+        ArrayAdapter<String> choiceBuildAdapter = new ArrayAdapter<String>(getSherlockActivity(),
+                R.layout.spinner_item_market_choice, new String[] {
+                getString(R.string.choice_ask_any),
+                getString(R.string.choice_ask_build_young_small),
+                getString(R.string.choice_ask_build_small),
+                getString(R.string.choice_ask_build_normal),
+                getString(R.string.choice_ask_build_big)
+                }
+                );
+        choiceBuildSpinner.setAdapter(choiceBuildAdapter);
 
         Button collectButton = (Button)rootView.findViewById(R.id.market_choice_submit_button);
         collectButton.setOnClickListener(new View.OnClickListener() {
@@ -69,22 +84,36 @@ public class MarketAskFragment extends SherlockFragment {
                         break;
                 }
                 switch (choiceSexSpinner.getSelectedItemPosition()) {
-                    case 0:
+                    case 1:
                         queryHashMap.put("Sex", getString(R.string.record_sex_female));
                         break;
-                    case 1:
+                    case 2:
                         queryHashMap.put("Sex", getString(R.string.record_sex_male));
                         break;
                 }
                 switch (choiceAgeSpinner.getSelectedItemPosition()) {
-                    case 0:
+                    case 1:
                         queryHashMap.put("Age", getString(R.string.choice_ask_age_young));
                         break;
-                    case 1:
+                    case 2:
                         queryHashMap.put("Age", getString(R.string.choice_ask_age_adult));
                         break;
-                    case 2:
+                    case 3:
                         queryHashMap.put("Age", getString(R.string.choice_ask_age_old));
+                        break;
+                }
+                switch (choiceBuildSpinner.getSelectedItemPosition()) {
+                    case 1:
+                        queryHashMap.put("Build", getString(R.string.choice_ask_build_young_small));
+                        break;
+                    case 2:
+                        queryHashMap.put("Build", getString(R.string.choice_ask_build_small));
+                        break;
+                    case 3:
+                        queryHashMap.put("Build", getString(R.string.choice_ask_build_normal));
+                        break;
+                    case 4:
+                        queryHashMap.put("Build", getString(R.string.choice_ask_build_big));
                         break;
                 }
                 FragmentManager fragmentManager = getSherlockActivity().getSupportFragmentManager();
